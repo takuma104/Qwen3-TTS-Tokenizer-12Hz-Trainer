7811d81ebe4736597206c7c3dbb14655e9294ff9
Takuma Mori <takuma104@gmail.com>
2026-03-17 22:07:36 +0900

    run15

--- train.sh
+++ train.sh
@@ -9,13 +9,13 @@ PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 TRAIN_SHARDS="${SCRIPT_DIR}/datasets/train/*.tar"
 VAL_SHARDS="${SCRIPT_DIR}/datasets/val/*.tar"
 OUTPUT_DIR="${SCRIPT_DIR}/output"
-RUN_NUMBER=14
+RUN_NUMBER=15
 
 uv run accelerate launch "${SCRIPT_DIR}/src/trainer.py" \
     --train_shards "${TRAIN_SHARDS}" \
     --val_shards "${VAL_SHARDS}" \
     --output_dir "${OUTPUT_DIR}/run${RUN_NUMBER}" \
-    --resume_from "${OUTPUT_DIR}/run10/checkpoint-step-186250" \
+    --resume_from "${OUTPUT_DIR}/run10/checkpoint-best" \
     --batch_size 8 \
     --num_decoder_block_frozen 0 \
     --lr_g 1e-5 \
@@ -28,6 +28,7 @@ uv run accelerate launch "${SCRIPT_DIR}/src/trainer.py" \
     --lambda_fm 1.0 \
     --lambda_multi_res_mel 15.0 \
     --lambda_global_rms 1.0 \
+    --lambda_d_mpd 0.01 \
     --lambda_d_msd 0.1 \
     --save_every 1250 \
     --eval_every 250 \
