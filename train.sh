0c0a439b6c2e808a04c168f3f1ac453b2f885291
Takuma Mori <takuma104@gmail.com>
2026-03-19 22:12:06 +0900

    run19

--- train.sh
+++ train.sh
@@ -9,16 +9,14 @@ PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 TRAIN_SHARDS="${SCRIPT_DIR}/datasets/train/*.tar"
 VAL_SHARDS="${SCRIPT_DIR}/datasets/val/*.tar"
 OUTPUT_DIR="${SCRIPT_DIR}/output"
-RUN_NUMBER=18
+RUN_NUMBER=19
 
 uv run accelerate launch "${SCRIPT_DIR}/src/trainer.py" \
     --train_shards "${TRAIN_SHARDS}" \
     --val_shards "${VAL_SHARDS}" \
     --output_dir "${OUTPUT_DIR}/run${RUN_NUMBER}" \
     --resume_from "${OUTPUT_DIR}/run10/checkpoint-best" \
-    --no_resume_optimizer \
     --batch_size 8 \
-    --train_full_decoder \
     --num_decoder_block_frozen 0 \
     --lr_g 1e-5 \
     --lr_d 2e-5 \
@@ -26,10 +24,11 @@ uv run accelerate launch "${SCRIPT_DIR}/src/trainer.py" \
     --max_train_steps 1000000 \
     --gradient_accumulation_steps 4 \
     --max_audio_length 3.0 \
-    --lambda_adv 0.3 \
-    --lambda_fm 3.0 \
-    --lambda_multi_res_mel 15.0 \
-    --lambda_global_rms 1.0 \
+    --log_grad_norms \
+    --lambda_adv 0.1 \
+    --lambda_fm 12.0 \
+    --lambda_multi_res_me 7.5 \
+    --lambda_global_rms 0.1 \
     --lambda_d_mpd 0.01 \
     --lambda_d_msd 0.1 \
     --save_every 1250 \
